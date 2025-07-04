517417c133322489deb516fbdd5db4c312209e0a
smailly-guy <artline2010k@gmail.com>
2025-07-04 21:34:54 +0300

    Add files via upload

---
+++ Program_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public delegate void MathMessage(string msg, int result);
+
+class SimpleMath
+{
+    private MathMessage handler;
+
+    public void SetMathHandler(MathMessage target)
+    {
+        handler = target;
+    }
+
+    public void Add(int x, int y)
+    {
+        handler?.Invoke("Adding has completed!", x + y);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SimpleMath m = new SimpleMath();
+        m.SetMathHandler((msg, result) =>
+            Console.WriteLine($"Message: {msg}, Result: {result}"));
+        m.Add(10, 10);
+    }
+}
